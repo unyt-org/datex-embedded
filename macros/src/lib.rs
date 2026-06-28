cf6081e345d644d46b20a8d0725ba1fa42282029
Jonas Strehle <jonas@strehles.info>
2026-06-28 12:33:41 +0200

    wip

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,9 +1,9 @@
 use proc_macro::{TokenStream};
 use proc_macro2::TokenStream as TokenStream2;
-use syn::{FnArg, ItemFn, Pat, Signature, Token, parse::{Parse, ParseStream}, parse_macro_input, parse_quote, Stmt};
+use syn::{FnArg, ItemFn, Pat, Signature, parse_macro_input, parse_quote};
 use quote::quote;
 use datex_core::{runtime::RuntimeConfig};
-use datex_macro_utils::entrypoint::{datex_main_impl, datex_main_impl_with_config, get_config, DatexMainInput, ParsedAttributes};
+use datex_macro_utils::entrypoint::{datex_main_impl_with_config, get_config, DatexMainInput, ParsedAttributes};
 
 #[proc_macro_attribute]
 pub fn main(attr: TokenStream, item: TokenStream) -> TokenStream {
