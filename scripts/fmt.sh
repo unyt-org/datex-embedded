6c551d83a1994470b28aeadbd9b754eddf56ad92
Jonas Strehle <jonas@strehles.info>
2026-06-30 19:39:48 +0200

    fix fmt

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-cargo clippy -p datex-embedded --fix --features target_esp32s3
-cargo clippy --workspace --exclude datex-embedded --fix --allow-dirty
+cargo clippy -p datex-embedded --fix --features target_esp32s3 --target xtensa-esp32s3-none-elf --allow-dirty
+cargo clippy --workspace --exclude datex-embedded --fix --allow-dirty --target xtensa-esp32s3-none-elf --allow-dirty
 cargo fmt --all
 git commit -a -m "fmt"
